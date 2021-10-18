3b56a5616118eeeea7a1ecc5f0bdd6c6c10a60ee
gldev072 <harshthadani98286@gmail.com>
2021-10-18 17:41:34 +0530

    initial commit

---
+++ config/otp.php
@@ -0,0 +1,9 @@
+<?php 
+
+return [
+    'msg91' =>[
+        'authKey'=>env('MSG91_AUTH_KEY'),
+        'senderId'=>env('MSG91_SENDER_ID'),
+        'templateId'=>env('MSG91_TEMPLATE_ID'),
+    ],
+];
